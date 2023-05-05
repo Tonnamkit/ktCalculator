6c80229530c4e88d246395c078b18ca100f5e83c
Tonnamkit <kittipat.bual@mail.kmutt.ac.th>
2023-05-05 14:31:10 +0700

    add fun onDigit and fun onCLear

--- app/src/main/java/eu/learn/mycalculator/MainActivity.kt
+++ app/src/main/java/eu/learn/mycalculator/MainActivity.kt
@@ -3,15 +3,26 @@ package eu.learn.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvinput : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvinput = findViewById(R.id.tv_input)
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this,"Button Clicked",Toast.LENGTH_LONG).show()
+        tvinput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View){
+        tvinput?.text = ""
     }
 }
