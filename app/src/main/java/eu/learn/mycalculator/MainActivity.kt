0e96fdda95439263aa1ef43bc7cb84a73c5b530d
Tonnamkit <kittipat.bual@mail.kmutt.ac.th>
2023-05-05 14:46:39 +0700

    add fun onDecimalPoint

--- app/src/main/java/eu/learn/mycalculator/MainActivity.kt
+++ app/src/main/java/eu/learn/mycalculator/MainActivity.kt
@@ -10,6 +10,8 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private var tvinput : TextView? = null
+    var lastdot : Boolean = false
+    var lastNumeric : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +22,19 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View){
         tvinput?.append((view as Button).text)
+        lastNumeric = true
+        lastdot = false
     }
 
     fun onClear(view: View){
         tvinput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View){
+        if (lastNumeric && !lastdot){
+            tvinput?.append(".")
+            lastNumeric = false
+            lastdot = true
+        }
     }
 }
